c1cc7a6d1cc2ca71b328ac02b46986826f7fea5c
Ashen Nethsara <ashenmawela7@gmail.com>
2026-06-20 19:22:42 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://drive.google.com/file/d/1KI9jEBpqeRi7QDIrANSnY54T8AzKiEOq/view?usp=sharing",
               },
               caption: sid,
             });
